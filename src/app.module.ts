fcc1671b681db738059f92cf40aa984ac5f02a61
송유현 <doong3373@gmail.com>
2021-07-20 13:39:25 +0900

    [ADD] channels, workspace, users, dms

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { LoggerMiddleware } from './middlewares/logger.middlewares';
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspace/workspace.module';
+import { ChannelsModule } from './channels/channels.module';
+import { DmsModule } from './dms/dms.module';
+import { WorkspaceController } from './workspace/workspace.controller';
+import { WorkspaceService } from './workspace/workspace.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), UsersModule, WorkspacesModule, ChannelsModule, DmsModule],
+  controllers: [AppController, WorkspaceController],
+  providers: [AppService, WorkspaceService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
---
+++ src/users/users.controller.ts
@@ -0,0 +1,4 @@
+import { Controller, Get, Post, Req } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {}
