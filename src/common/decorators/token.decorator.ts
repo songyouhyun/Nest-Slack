51929242c9029cf40a98be12c2f2ce3699f921a3
송유현 <doong3373@gmail.com>
2021-07-21 14:51:58 +0900

    ctx 관련 주석 추가

--- src/common/decorators/token.decorator.ts
+++ src/common/decorators/token.decorator.ts
@@ -11,5 +11,6 @@ export const Token = createParamDecorator(
 // @Token token
 
 // ctx(실행컨텍스트)가 쓰이는 이유
-// ctx가 HTTP, WebSocket, RPC 등 이 세 개를 하나의 객체로 접근하기 위해
+// Nest로 한 서버에서 여러가지(HTTP, RPC, WebSocket)등을 동시에 돌릴 수 있는데, 모두다 하나의 실행컨텍스트 안에서 관리를 한다.
+// ctx는 HTTP, WebSocket, RPC 등 이 세 개를 하나의 객체로 접근하기 위해 사용된다.
 // 지금 이 Slack 클론 코딩에서는 실시간 채팅을 만들때 WebSocket과 HTTP 서버가 같이 돌아간다.
