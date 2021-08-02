d208e14d53c5071a468bec1b3239f331ce948443
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-08-02 20:55:34 +0900

    fix(local.strategy): passport Strategy import error 해결

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { Strategy } from 'passport-local';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
