b40643d8f44ae10d8ea35064065f89f2c4686e72
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-08-05 19:15:16 +0900

    ✨ Feat: users
    Workspace에 사용자 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,20 +7,27 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from 'src/entities/Users';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { WorkspaceMembers } from '../entities/WorkspaceMembers';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users)
     private usersRepository: Repository<Users>,
+    @InjectRepository(WorkspaceMembers)
+    private workspaceMembersRepository: Repository<WorkspaceMembers>,
   ) {}
   getUser() {}
   async join(email: string, nickname: string, password: string) {
     const hashedPassword = await bcrypt.hash(password, 12);
-    await this.usersRepository.save({
+    const joinedUser = await this.usersRepository.save({
       email,
       nickname,
       password: hashedPassword,
+    });
+    await this.workspaceMembersRepository.save({
+      UserId: joinedUser.id,
+      WorkspaceId: 1,
     });
   }
 }
