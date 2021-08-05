f2d61ebb6009cac1833ea30b40e383aef562fcb6
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-08-05 23:25:06 +0900

    ✨ Feat: user.service
    channelMember에 사용자 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { Users } from 'src/entities/Users';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { WorkspaceMembers } from '../entities/WorkspaceMembers';
+import { ChannelMembers } from '../entities/ChannelMembers';
 
 @Injectable()
 export class UsersService {
@@ -12,8 +13,11 @@ export class UsersService {
     private usersRepository: Repository<Users>,
     @InjectRepository(WorkspaceMembers)
     private workspaceMembersRepository: Repository<WorkspaceMembers>,
+    @InjectRepository(ChannelMembers)
+    private channelMembersRepository: Repository<ChannelMembers>,
   ) {}
   getUser() {}
+  // 사용자를 만들고, 바로 workspace와 channel에 들어가게 하는 method
   async join(email: string, nickname: string, password: string) {
     const hashedPassword = await bcrypt.hash(password, 12);
     const joinedUser = await this.usersRepository.save({
@@ -21,9 +25,15 @@ export class UsersService {
       nickname,
       password: hashedPassword,
     });
+    // seeding된 첫번째 workspace인 'slect'에다가, 위에서 회원가입된 user를 넣어주는 save문
     await this.workspaceMembersRepository.save({
       UserId: joinedUser.id,
       WorkspaceId: 1,
     });
+    await this.channelMembersRepository.save({
+      UserId: joinedUser.id,
+      ChannelId: 1,
+    });
+    return true;
   }
 }
