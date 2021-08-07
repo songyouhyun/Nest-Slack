b5485eedb72e1cb36ba7a2427ee9842b7117befa
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-08-07 23:57:08 +0900

    🐛 Fix: Workspaces.module
    forgotten import

--- src/workspaces/workspaces.module.ts
+++ src/workspaces/workspaces.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { WorkspacesService } from './workspaces.service';
 import { WorkspacesController } from './workspaces.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WorkspaceMembers } from '../entities/WorkspaceMembers';
+import { Workspaces } from '../entities/Workspaces';
+import { ChannelMembers } from '../entities/ChannelMembers';
+import { Channels } from '../entities/Channels';
+import { Users } from '../entities/Users';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      WorkspaceMembers,
+      Workspaces,
+      ChannelMembers,
+      Channels,
+      Users,
+    ]),
+  ],
   controllers: [WorkspacesController],
-  providers: [WorkspacesService]
+  providers: [WorkspacesService],
 })
 export class WorkspacesModule {}
