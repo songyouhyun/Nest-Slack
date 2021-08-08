695351a11e1075df52d252a5fb5a3104d3e6860e
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-08-08 23:58:03 +0900

    ✨ Feat: workspaces.service
    findById, findMyWorkspaces method 추가

--- src/workspaces/workspaces.service.ts
+++ src/workspaces/workspaces.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Workspaces } from '../entities/Workspaces';
+import { Repository } from 'typeorm';
+import { Channels } from '../entities/Channels';
+import { WorkspaceMembers } from '../entities/WorkspaceMembers';
+import { ChannelMembers } from '../entities/ChannelMembers';
+import { Users } from '../entities/Users';
 
 @Injectable()
-export class WorkspacesService {}
+export class WorkspacesService {
+  constructor(
+    @InjectRepository(Workspaces)
+    private workspacesRepository: Repository<Workspaces>,
+    @InjectRepository(Channels)
+    private channelsRepository: Repository<Channels>,
+    @InjectRepository(WorkspaceMembers)
+    private workspaceMembersRepository: Repository<WorkspaceMembers>,
+    @InjectRepository(ChannelMembers)
+    private channelMembersRepository: Repository<ChannelMembers>,
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>,
+  ) {}
+
+  async findById(id: number) {
+    return this.workspacesRepository.findByIds([id]);
+  }
+
+  async findMyWorkspaces(myId: number) {
+    return this.workspacesRepository.find({
+      where: { WorkspaceMembers: [{ UserId: myId }] },
+    });
+  }
+}
