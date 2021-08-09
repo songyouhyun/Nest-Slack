4062bc8ccd20253176b63190add8ea1b8819e87b
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-08-10 00:05:36 +0900

    ✨ Feat: workspaces.controller
    controller에 User decorator 사용하여 getMyWorkspaces 생성

--- src/workspaces/workspaces.controller.ts
+++ src/workspaces/workspaces.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Delete, Get, Post } from '@nestjs/common';
 import { WorkspacesService } from './workspaces.service';
 import { ApiTags } from '@nestjs/swagger';
+import { User } from '../common/decorators/user.decorator';
+import { Users } from '../entities/Users';
 
 @ApiTags('WORKSPACE')
 @Controller('api/workspaces')
@@ -8,32 +10,22 @@ export class WorkspacesController {
   constructor(private readonly workspacesService: WorkspacesService) {}
 
   @Get()
-  getMyWorkspaces() {
-
+  getMyWorkspaces(@User() user: Users) {
+    return this.workspacesService.findMyWorkspaces(user.id);
   }
 
   @Post()
-  createWorkspaces() {
+  createWorkspaces() {}
 
-  }
+  @Get(':url/members')
+  getAllmembersFromWorkspace() {}
 
-  @Get(":url/members")
-  getAllmembersFromWorkspace() {
+  @Post(':url/members')
+  inviteMembersToWorkspace() {}
 
-  }
+  @Delete(':url/members/:id')
+  kickMemberFromWorkspace() {}
 
-  @Post(":url/members")
-  inviteMembersToWorkspace() {
-
-  }
-
-  @Delete(":url/members/:id")
-  kickMemberFromWorkspace() {
-    
-  }
-
-  @Get(":url/members/:id")
-  getMemberInfoInWorkspace() {
-
-  }
+  @Get(':url/members/:id')
+  getMemberInfoInWorkspace() {}
 }
