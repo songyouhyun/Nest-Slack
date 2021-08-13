1dc9c229e5eb8a523d583bd2e18d01724b18e37e
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-08-13 17:29:24 +0900

    ✨ Feat: workspaces.service
    createWorkspace 기능 추가

--- src/workspaces/workspaces.service.ts
+++ src/workspaces/workspaces.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Workspaces } from '../entities/Workspaces';
-import { Repository } from 'typeorm';
+import { Repository, Transaction } from "typeorm";
 import { Channels } from '../entities/Channels';
 import { WorkspaceMembers } from '../entities/WorkspaceMembers';
 import { ChannelMembers } from '../entities/ChannelMembers';
@@ -30,5 +30,38 @@ export class WorkspacesService {
     return this.workspacesRepository.find({
       where: { WorkspaceMembers: [{ UserId: myId }] },
     });
+  }
+  @Transaction()
+  async createWorkspace(name: string, url: string, myId: number) {
+    // 새로운 workspace 생성
+    const workspace = this.workspacesRepository.create({
+      name,
+      url,
+      OwnerId: myId,
+    });
+    const returned = await this.workspacesRepository.save(workspace);
+    // 생성된 workSpace에 '나'(myId)를 추가 (1)
+    const workspaceMember = this.workspaceMembersRepository.create({
+      UserId: myId,
+      WorkspaceId: returned.id,
+    });
+    // '일반'이라는 이름의 새로운 Channel 생성
+    const channel = this.channelsRepository.create({
+      name: '일반',
+      WorkspaceId: returned.id,
+    });
+    // Workspace에 '나' 추가하는 것과 workspace에 channel을 만드는 것은 동시에 진행 (2)
+    // 즉, await를 이용한 비동기적 처리 말고 동기적으로 일을 처리하면 더 빠르다는 말
+    const [, channelReturned] = await Promise.all([
+      this.workspaceMembersRepository.save(workspaceMember),
+      this.channelsRepository.save(channel),
+    ]);
+    // 생성된 channel에 '나'(myId)를 추가
+    const channelMember = this.channelMembersRepository.create({
+      UserId: myId,
+      ChannelId: channelReturned.id,
+    });
+    // (3)
+    await this.channelMembersRepository.save(channelMember);
   }
 }
