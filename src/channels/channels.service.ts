e0f8b769f772beaca0512be432b9191ed8db2471
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-08-22 18:24:07 +0900

    ✨ Feat: channels.service.ts
    Inject Repository

--- src/channels/channels.service.ts
+++ src/channels/channels.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Channels } from '../entities/Channels';
+import { Repository } from 'typeorm';
+import { ChannelMembers } from '../entities/ChannelMembers';
+import { Workspaces } from '../entities/Workspaces';
+import { ChannelChats } from '../entities/ChannelChats';
+import { Users } from '../entities/Users';
 
 @Injectable()
-export class ChannelsService {}
+export class ChannelsService {
+  constructor(
+    @InjectRepository(Channels)
+    private channelsRepository: Repository<Channels>,
+    @InjectRepository(ChannelMembers)
+    private channelMembersRepository: Repository<ChannelMembers>,
+    @InjectRepository(ChannelChats)
+    private channelChatsRepository: Repository<ChannelChats>,
+    @InjectRepository(Workspaces)
+    private workspacesRepository: Repository<Workspaces>,
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>,
+  ) {}
+}
