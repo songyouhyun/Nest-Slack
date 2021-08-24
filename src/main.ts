3f57ebce2e7f8e98b9da3a37eb290ae27fd505c3
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-08-24 10:30:36 +0900

    ✨ Feat: main.ts (GlobalValidation)
    ValidationPipe 안에 transform: true로 설정

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,11 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Slack API')
